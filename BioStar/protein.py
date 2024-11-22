794309c5382e6f3e7ba9d390e4c0546fcc110b4f
Nicolas Martins <contato.sistemasmartins@gmail.com>
2024-11-22 00:26:23 -0300

    Added new comments
    This current update does not add nothing new, just improved the overall readability and clarity of the code.

--- BioStar/protein.py
+++ BioStar/protein.py
@@ -6,17 +6,46 @@ from BioStar.DataBiochemistry import (AMINOACIDS, AMINOACID_TABLE,
 
 class Protein:
     def __init__(self, sequence: str = "") -> None:
+        """
+        Summary:
+            Initializes the Protein object with a sequence and computes amino acid counts.
+
+        Parameters:
+            sequence (str): The protein sequence in FASTA format (default is an empty string).
+
+        Returns:
+            None
+        """
         self.sequence: str = self._fasta_sequence(sequence)
         self.count: dict = self._update_count()
         self.sequence_size = self.count["total"]
 
     def _fasta_sequence(self, sequence: str = "") -> str:
+        """
+        Summary:
+            Converts the input sequence into a valid FASTA format, removing non-amino acid characters.
+
+        Parameters:
+            sequence (str): The input protein sequence.
+
+        Returns:
+            str: The cleaned and uppercased sequence in FASTA format.
+        """
         fasta_sequence = sub(r'[^ACDEFGHIKLMNPQRSTVWY]', '', sequence.upper())
         return fasta_sequence
 
     def _update_count(self) -> dict:
-        count_by_aminoacid: dict = {aa: 0 for aa in AMINOACIDS}
+        """
+        Summary:
+            Updates and returns a dictionary with the counts of each type of amino acid in the protein sequence.
 
+        Parameters:
+            self
+
+        Returns:
+            dict: A dictionary containing counts for different categories of amino acids (e.g., aromatic, nonpolar, polar).
+        """
+        count_by_aminoacid: dict = {aa: 0 for aa in AMINOACIDS}
         count_total: int = 0
         count_aromatic: int = 0
         count_nonpolar: int = 0
@@ -38,17 +67,12 @@ class Protein:
                 count_by_aminoacid[aa] += 1
                 count_polar += 1
                 if aa in ["D", "E"]:
-                    # Aspartic Acid and Glumatic Acid; respectively
                     count_polar_negative += 1
                 elif aa in ["C", "N", "Q", "S", "T", "Y"]:
-                    # Cystein, Asparagine, Glutamine, Serine, Threonine, Tyrosine
                     count_polar_neutral += 1
                 elif aa in ["H", "K", "R"]:
                     count_polar_positive += 1
-                    # Histidine, Lysine, Arginine; respectively
-
                 count_total += 1
-
         
         aminoacid_count: dict = {
             'by_aminoacid': count_by_aminoacid,
@@ -64,13 +88,29 @@ class Protein:
         return aminoacid_count
 
     def aromacity(self, multiply_by: float = 1.0) -> float:
+        """
+        Summary:
+            Calculates the aromacity of the protein, defined as the fraction of aromatic residues.
+
+        Parameters:
+            multiply_by (float): A multiplier to scale the result (default is 1.0).
+
+        Returns:
+            float: The aromacity of the protein, scaled by the multiplier.
+        """
         aromacity: float = self.count['aromatic'] / self.count['total']
         return round((aromacity * multiply_by), 1)
 
     def charge_at_pH(self, pH: float) -> float:
         """
-        This function takes the pH as a parameter.
-        Returns the floating point value of the estimated charge of a protein.
+        Summary:
+            Calculates the charge of the protein at a given pH based on its amino acid composition.
+
+        Parameters:
+            pH (float): The pH at which the protein charge is to be calculated.
+
+        Returns:
+            float: The net charge of the protein at the given pH, rounded to two decimal places.
         """
         normalized_pH: float = round(pH, 3)
         positive_charge: float = 0.0
@@ -110,10 +150,16 @@ class Protein:
         return round(positive_charge - negative_charge, 2)
 
     def composition_ratio(self, multiply_by: float = 1.0) -> dict:
-        '''
-        Method which returns the ratio composition of all aminoacids.
-        By default the sum adds up to 1 but you can change the multiplier.
-        '''
+        """
+        Summary:
+            Calculates the composition ratio of amino acids in the protein sequence.
+
+        Parameters:
+            multiply_by (float): A multiplier to scale the composition ratios (default is 1.0).
+
+        Returns:
+            dict: A dictionary of amino acid composition ratios, scaled by the multiplier.
+        """
         total = self.count["total"]
         composition_summary = {aa: round((count / total) * multiply_by, 2) for aa, count in self.count['by_aminoacid'].items()}
         return composition_summary
@@ -121,18 +167,17 @@ class Protein:
     def extinction_coefficient(self) -> dict:
         """
         Summary:
-        Calculate the extinction coefficient of the protein at 280 nm.
-        Based on the number of Trp, Tyr, and Cys residues.
+            Calculates the extinction coefficient of the protein at 280 nm.
+            Based on the number of Trp, Tyr, and Cys residues.
 
         Parameters:
             self
 
         Returns:
             dict: The extinction coefficient (in M^-1 cm^-1).
-                cys_cystines assumes all cys residues form cystines
-                cys_reduced assumes all cys residues are reduced
+                cys_cystines assumes all cysteine residues form cystines.
+                cys_reduced assumes all cysteine residues are reduced.
         """
-        # Aromatic residues contributing to absorbance
         c_count = self.sequence.count("C")  # Cysteine
         w_count = self.sequence.count("W")  # Tryptophan
         y_count = self.sequence.count("Y")  # Tyrosine
@@ -159,13 +204,32 @@ class Protein:
         }
 
         return coefficients
-    
+
+    def hydrophobic_index(self) -> float:
+        """
+        Summary:
+            Calculates the hydrophobic index of the protein based on the hydrophobicity of each amino acid.
+
+        Parameters:
+            self
+
+        Returns:
+            float: The average hydrophobicity of the protein, rounded to two decimal places.
+        """
+        h_index = sum(AMINOACID_TABLE[aa]['hydrophobicity'] for aa in self.sequence)
+        return round(h_index / len(self.sequence), 2)
+
     def isoelectric_point(self) -> float:
         """
-        Returns the floating point value of the isoelectric point (pI) of the protein.
-        The pI is the pH at which the protein's net charge is zero.
+        Summary:
+            Calculates the isoelectric point (pI) of the protein, the pH at which the net charge of the protein is zero.
+
+        Parameters:
+            self
+
+        Returns:
+            float: The estimated isoelectric point (pI) of the protein, rounded to two decimal places.
         """
-        # Set a range for pH values and a tolerance for charge
         low_pH = 0.0
         high_pH = 14.0
         tolerance = 0.01  # Acceptable deviation for "zero charge"
@@ -183,20 +247,18 @@ class Protein:
 
         # Return the midpoint as the best estimate of the pI
         return round((low_pH + high_pH) / 2.0, 2)
-    
-    def hydrophobic_index(self) -> float:
-        '''
-        Hydrophic index calculated by the sum of the hydrophobicity
-        of every aminoacid present in the protein
-        '''
-        h_index = sum(AMINOACID_TABLE[aa]['hydrophobicity'] for aa in self.sequence)
-        return round(hydrophobicity / len(self.sequence), 2)
 
     def molecular_weight(self) -> float:
-        '''
-        Sum the weight of every aminoacid present in the protein
-        '''
-        
+        """
+        Summary:
+            Calculates the molecular weight of the protein by summing the weights of its amino acids.
+
+        Parameters:
+            self
+
+        Returns:
+            float: The molecular weight of the protein, with the mass of water subtracted.
+        """
         molecular_weight = sum(AMINOACID_TABLE[aa]['weight'] for aa in self.sequence)
 
         # Subtract water from the amount of peptide bonds
@@ -205,13 +267,16 @@ class Protein:
         return round(molecular_weight, 2)
 
     def secondary_structure_propensity(self) -> dict:
-        '''
-        Function returns a dictionary of alpha_helix, beta_sheet or coil propensity
+        """
+        Summary:
+            Calculates the propensity for secondary structures (alpha helix, beta sheet, coil) in the protein.
 
-        Propensity for secondary structures represents an intrinsic property of amino acid,
-        and it is used for generating new algorithms and prediction methods.
-        '''
+        Parameters:
+            self
 
+        Returns:
+            dict: A dictionary containing the percentage propensity for alpha helix, beta sheet, and coil.
+        """
         alpha_helix_propensity = sum(AMINOACID_TABLE[aa]['alpha_helix'] for aa in self.sequence) / len(self.sequence)
         beta_sheet_propensity = sum(AMINOACID_TABLE[aa]['beta_sheet'] for aa in self.sequence) / len(self.sequence)
         coil_propensity = 1 - (alpha_helix_propensity + beta_sheet_propensity)  # Simplified assumption
